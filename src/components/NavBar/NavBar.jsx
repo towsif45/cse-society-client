9bd9ac5e3c75d46875a547b57a8f3dca634173c5
Towsif Hossain <towsifhossain5120@gmail.com>
2021-12-01 17:41:03 +0600

    initial commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { AppBar, Avatar, Box, Toolbar, Typography } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { PostAdd, Search } from "@mui/icons-material";
 import { Searchbox } from "./style/searchbar/Searchbox";
 import { StyledInputBase } from "./style/searchbar/StyledInputBase";
 import { SearchIconWrapper } from "./style/searchbar/SearchIconWrapper";
 import { Iconbox } from "./style/iconbar/Iconbox";
-import { deepOrange } from "@mui/material/colors";
 
 const NavBar = () => {
     return (
@@ -30,8 +29,7 @@ const NavBar = () => {
                 </Searchbox>
                 <Box sx={{ flexGrow: 1 }} />
                 <Iconbox>
-                    <Box sx={{ width: '1em'}} />
-                    <Avatar sx={{ bgcolor: deepOrange[500] }}> T </Avatar>
+                    <Button variant="contained" disableElevation startIcon={<PostAdd /> }>Create post</Button>
                 </Iconbox>
             </Toolbar>
         </AppBar>
