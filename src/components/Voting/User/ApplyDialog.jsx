00e9e46eced441b3c6714950e97168a97f4217a9
Towsif Hossain <towsifhossain5120@gmail.com>
2021-12-03 08:28:58 +0600

    initialcommit

--- src/components/Voting/User/ApplyDialog.jsx
+++ src/components/Voting/User/ApplyDialog.jsx
@@ -1,4 +1,6 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from "@mui/material";
+
+const positions = ['Vice President', 'General Secretary', 'Assistant General Secretary', 'Executive Member'];
 
 const ApplyDialog = (props) => {
     const {onClose, open, data} = props;
@@ -8,12 +10,27 @@ const ApplyDialog = (props) => {
     }
 
     return(
-        <Dialog open={open} onClose={handleClose} >
+        <Dialog open={open} onClose={handleClose} fullWidth maxWidth='sm'>
             <DialogTitle>
                 <Typography variant="h6">Apply for a position</Typography>
             </DialogTitle>
             <DialogContent>
-
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Positions</FormLabel>
+                    <RadioGroup
+                        aria-label="gender"
+                        name="radio-buttons-group"
+                    >
+                        {
+                            positions.map((pos) => (
+                                <FormControlLabel value={pos} control={<Radio />} label={pos} />
+                            ))
+                        }
+                        {/* <FormControlLabel value="female" control={<Radio />} label="Female" />
+                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                        <FormControlLabel value="other" control={<Radio />} label="Other" /> */}
+                    </RadioGroup>
+                </FormControl>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>Cancel</Button>
